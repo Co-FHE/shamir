ecd539b030542fab876ca691b30d93e3c72ee79f
shiotoli <shiotoli@gmail.com>
2025-02-07 15:20:24 +0800

    expose interface

--- tss-sdk/src/lib.rs
+++ tss-sdk/src/lib.rs
@@ -1,6 +1,7 @@
 pub use common::Settings;
 pub mod crypto {
     pub use coordinator_signer::crypto::validator_identity::p2p_identity::P2pIdentity;
+    pub use coordinator_signer::crypto::validator_identity::sr25519_identity::Sr25519Identity;
     pub use coordinator_signer::crypto::PkId;
 }
 pub mod node {

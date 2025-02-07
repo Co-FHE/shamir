f1d4b604bf26d582f86c0e77029e57d95adf167e
shiotoli <shiotoli@gmail.com>
2025-02-07 17:47:50 +0800

    expose api

--- tss-sdk/src/lib.rs
+++ tss-sdk/src/lib.rs
@@ -3,6 +3,7 @@ pub mod crypto {
     pub use coordinator_signer::crypto::validator_identity::p2p_identity::P2pIdentity;
     pub use coordinator_signer::crypto::validator_identity::sr25519_identity::Sr25519Identity;
     pub use coordinator_signer::crypto::PkId;
+    pub use coordinator_signer::crypto::ValidatorIdentity;
 }
 pub mod node {
     pub use coordinator_signer::node::Node;
